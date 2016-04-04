449126ab084f678fe8b8ed2bd20288362cf0d8a4
Tero Ahonen <tero.ahonen@cybercom.com>
2016-04-04 12:24:50 +0300

    created index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<html> 
+<head>
+<title>Hello OpenStackers!</title>
+</head>
+<body style="background-color: red">
+<h2>
+<?php echo "Hello OpenStackers! My version is v1.";?>
+</h2>
+<p>
+<?php echo "My Pod IP is : ".$_SERVER['SERVER_ADDR'] ;?>
+</p>
+</body>
+</html>
